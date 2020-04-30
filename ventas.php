0fff065efe5b7a40ec6c21a576284c0defa18240
rodrigo1985-1997 <61755483+rodrigo1985-1997@users.noreply.github.com>
2020-04-30 01:11:28 -0500

    Add files via upload

---
+++ ventas.php
@@ -0,0 +1,19 @@
+<H1>Calcular el salario de un vendedor</H1></CENTER>
+<?php
+ 
+ $nombre=$_POST['nombre'];
+ $cantidad=$_POST['cantidad'];
+ $precioTotal=$_POST['precioTotal'];
+ 
+ echo "El nombre del vendedor es: ".$nombre."<br>";
+  echo "La cantidad de automoviles vendidos es: ".$cantidad."<br>";
+  echo "El precio total de automoviles vendidos es:".$precioTotal."<br>";
+ 
+ $salarioBasico=450000;
+ $comision=$cantidad*50000;
+ $porcentajeVenta=$precioTotal*0.05;
+ $salario=$salarioBasico+$comision+$porcentajeVenta;
+
+ echo "El salario del vendedor es: ".$salario."<br>";
+ 
+?>
